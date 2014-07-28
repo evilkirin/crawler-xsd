0c549b3981ac897f95acc1407fd02405944d9a74
燮羽 <xieyi.xie@alibaba-inc.com>
2014-07-28 15:12:32 +0800

    minor

---
+++ src/test/java/org/alibaba/words/core/WeiboCrawlerTest.java
@@ -0,0 +1,13 @@
+package org.alibaba.words.core;
+
+import org.junit.Test;
+
+
+public class WeiboCrawlerTest {
+
+	@Test
+	public void testQueryWeiboList() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
